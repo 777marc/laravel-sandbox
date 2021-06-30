9136690df0038909fbe7ffe795f4aab92e458c15
777marc@gmail.com <777marc@gmail.com>
2021-06-30 11:54:22 -0400

    misc

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
     //event(new GiftCertificatePurchased());
     return view('welcome');
 });
+
+Route::get('/foo', function () {
+    dd('heyo test');
+})->middleware('api');
